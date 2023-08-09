5f3d1974055d56e8db40daf189ca111ca8cd9ae4
“Zane <zlh4@duke.edu>
2023-08-08 22:05:51 -0400

    Added image and text to about me section

--- app/components/AboutMeSection.js
+++ app/components/AboutMeSection.js
@@ -4,13 +4,20 @@ import Image from "next/image";
 export default function AboutMeSection() {
   return (
     <div className={aboutMeStyles.container} id="about-me-container">
-      <div id="about-me-text"></div>
+      <div className={aboutMeStyles["about-me-text"]} id="about-me-text">
+        <h2>About Me</h2>
+        <h3>
+          This is where I'll put some about me text. Talk about my education,
+          interests, and whatever else I feel like putting here!
+        </h3>
+      </div>
       <div id="about-me-image">
         <Image
-          src="/../../public/headshot.jpg"
-          width={200}
-          height={200}
+          src="/headshot.jpg"
+          width={300}
+          height={300}
           alt="Photo of Zane Harrison"
+          className={aboutMeStyles.headshot}
         />
       </div>
     </div>
--- app/components/Header.js
+++ app/components/Header.js
@@ -6,22 +6,22 @@ export default function Header() {
   return (
     <div className={headerStyles["flex-container"]}>
       <div className={headerStyles["flex-child"]}>
-        <Link href="/">
+        <Link href="/" legacyBehavior>
           <button className={buttonStyles["nav-button"]}>Home</button>
         </Link>
       </div>
       <div className={headerStyles["flex-child"]}>
-        <Link href="/about">
+        <Link href="/about" legacyBehavior>
           <button className={buttonStyles["nav-button"]}>About</button>
         </Link>
       </div>
       <div className={headerStyles["flex-child"]}>
-        <Link href="/about">
+        <Link href="/about" legacyBehavior>
           <button className={buttonStyles["nav-button"]}>Something</button>
         </Link>
       </div>
       <div className={headerStyles["flex-child"]}>
-        <Link href="/about">
+        <Link href="/about" legacyBehavior>
           <button className={buttonStyles["nav-button"]}>Something</button>
         </Link>
       </div>
