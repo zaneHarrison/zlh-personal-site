5bc02017b427610d9413c7b4dfa99c21d6e35133
“Zane <zlh4@duke.edu>
2023-08-10 22:25:02 -0400

    Added <hr> tag and Download Resume button to About Me text

--- app/components/AboutMeSection.js
+++ app/components/AboutMeSection.js
@@ -6,10 +6,18 @@ export default function AboutMeSection() {
     <div className={aboutMeStyles.container} id="about-me-container">
       <div className={aboutMeStyles["about-me-text"]} id="about-me-text">
         <h1>About Me</h1>
+        <hr className={aboutMeStyles["horizontal-line"]}></hr>
         <p className={aboutMeStyles["about-me-p-text"]}>
           This is where I'll put some about me text. Talk about my education,
           interests, and whatever else I feel like putting here!
         </p>
+        <a
+          className={aboutMeStyles["download-resume"]}
+          href="/ZaneHarrisonResume.pdf"
+          download
+        >
+          Download Resume
+        </a>
       </div>
       <div id="about-me-image">
         <Image
