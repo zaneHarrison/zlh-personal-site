43fec2f1dca75eb6b4e70e838fd35c6f8e31dc06
“Zane <zlh4@duke.edu>
2023-08-12 15:44:32 -0400

    Added link bar section, updated resume button

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import LandingPageGreeting from "./components/LandingPageGreeting";
 import AboutMeSection from "./components/AboutMeSection";
-import DownloadResumeSection from "./components/DownloadResumeSection";
+import ViewResumeSection from "./components/ViewResumeSection";
+import LinksBar from "./components/LinksBar";
 
 export default function Home() {
   return (
     <main>
       <LandingPageGreeting />
       <AboutMeSection />
-      <DownloadResumeSection />
+      <LinksBar />
+      <ViewResumeSection />
     </main>
   );
 }
