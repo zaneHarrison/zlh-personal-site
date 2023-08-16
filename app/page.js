8c9421f10229d88d22f52b3f9e5736a1b9a61321
“Zane <zlh4@duke.edu>
2023-08-15 22:42:46 -0400

    Added formatting message to appear on mobile devices

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import LandingPageGreeting from "./components/LandingPageGreeting";
 import AboutMeSection from "./components/AboutMeSection";
 import ViewResumeSection from "./components/ViewResumeSection";
 import LinksBar from "./components/LinksBar";
+import MobileMessage from "./components/MobileMessage";
 
 export default function Home() {
   return (
     <main>
+      <MobileMessage />
       <LandingPageGreeting />
       <AboutMeSection />
       <LinksBar />
