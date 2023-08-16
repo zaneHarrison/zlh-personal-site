5f6babc60ab965e20a238edb78bbb14bd8a0eb7e
“Zane <zlh4@duke.edu>
2023-08-16 19:34:59 -0400

    Added title to interests section

--- app/components/InterestsSection.js
+++ app/components/InterestsSection.js
@@ -1,5 +1,9 @@
 import interestsStyles from "../../styles/InterestsSection.module.css";
 
 export default function InterestsSection() {
-  return <div className={interestsStyles.container}></div>;
+  return (
+    <div className={interestsStyles.container}>
+      <h1 className={interestsStyles.title}>Some Things I Find Interesting</h1>
+    </div>
+  );
 }
