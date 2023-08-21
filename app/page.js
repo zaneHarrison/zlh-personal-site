3d5e119dd968db9044f468597380cdb7a818fec0
“Zane <zlh4@duke.edu>
2023-08-21 17:54:01 -0500

    Commented out interests section to work on it

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
     <main>
       <LandingPageGreeting />
       <AboutMeSection />
-      <InterestsSection />
+      {/* <InterestsSection /> */}
       <LinksBar />
     </main>
   );
