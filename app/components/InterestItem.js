d221c385edc4845962f6ba1a92df0c8829cbd89e
“Zane <zlh4@duke.edu>
2023-08-23 15:27:06 -0500

    Made changes to list of interest items

--- app/components/InterestItem.js
+++ app/components/InterestItem.js
@@ -1,9 +1,12 @@
+import Link from "next/link";
 import styles from "../../styles/InterestItem.module.css";
 
-export default function InterestItem({ text }) {
+export default function InterestItem({ text, path }) {
   return (
-    <div className={styles.container}>
-      <p>{text}</p>
+    <div className={styles["interest-item"]}>
+      <Link className={styles.link} href={path}>
+        {text}
+      </Link>
     </div>
   );
 }
--- app/components/InterestsSection.js
+++ app/components/InterestsSection.js
@@ -2,12 +2,14 @@ import interestsStyles from "../../styles/InterestsSection.module.css";
 import InterestItem from "./InterestItem";
 
 export default function InterestsSection() {
-  const videosText = "test";
-
   return (
     <div className={interestsStyles.container}>
       <h1 className={interestsStyles.title}>Some Things I Find Interesting</h1>
-      <InterestItem text={videosText} />
+      <div className={interestsStyles["interest-item-list"]}>
+        <InterestItem text="YouTube Videos" path="/" />
+        <InterestItem text="Reading" path="/" />
+        <InterestItem text="Music" path="/" />
+      </div>
     </div>
   );
 }
